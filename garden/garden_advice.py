175ab6e5dd4460427ab15dda5abb798f24d02dbb
Cady <mcmerkle@gmail.com>
2025-05-04 10:36:59 -0800

    added plant recommendations for season and plant type

--- garden/garden_advice.py
+++ garden/garden_advice.py
@@ -1,6 +1,8 @@
-# Hardcoded values for the season and plant type
-season = "summer"  # TODO: Replace with input() to allow user interaction.
-plant_type = "flower"  # TODO: Replace with input() to allow user interaction.
+"""Issue one: remove hardcoded variable values
+- Replaced season = "summer" variable and added user input
+- Replaced plant_type = "flower" variable and added user input"""
+season = input("Choose a season: winter or summer: ").lower()
+plant_type = input("Choose either flower or vegetable: ").lower()
 
 # Variable to hold gardening advice
 advice = ""
@@ -21,6 +23,21 @@ elif plant_type == "vegetable":
 else:
     advice += "No advice for this type of plant."
 
+"""Issue two: add plant recommendations based on
+season and plant type choices"""
+# Determine plant recommendation based on the season and plant type
+if plant_type == "flower" and season == "summer":
+    advice += "Summer flower recommendations: Sunflower, Azaleas,"
+    " and Peace Lillies"
+elif plant_type == "flower" and season == "winter":
+    advice += "Winter flower recommendations: Snapdragons,"
+    " Hyacinth, and Witch Hazel"
+elif plant_type == "vegetable" and season == "summer":
+    advice += "Summer vegetable recommendations: Corn, Green Beans, Tomatoes"
+elif plant_type == "vegetable" and season == "winter":
+    advice += "Winter vegetable recommendations: Acorn Squash,"
+    " Sweet Potatoes, Turnips"
+
 # Print the generated advice
 print(advice)
 
@@ -28,4 +45,3 @@ print(advice)
 # - Add detailed comments explaining each block of code.
 # - Refactor the code into functions for better readability and modularity.
 # - Store advice in a dictionary for multiple plants and seasons.
-# - Recommend plants based on the entered season.
